8f199a1f514ae5d8d75c658e6c5b79418cf51441
Scott Hale <sth0067@gmail.com>
2023-09-15 14:59:59 -0500

    added styling example to add cart button, added daisyui dependency

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -9,6 +9,10 @@ const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
     cache: 'no-store',
   });
+  // the second argument in the fetch function is an options object
+  // the cache: 'no-store' option will disable caching, usefull if you have data that changes frequently
+  // the next: { revalidate: 10 } option will get fresh data from the back-end every ten seconds
+
   const users: User[] = await res.json();
 
   return (
