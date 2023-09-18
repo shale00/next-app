34353c7a166bce9dbe2f1364630f374607383a90
Scott Hale <sth0067@gmail.com>
2023-09-17 23:06:58 -0500

    added programmatic navigation to Users, create user takes you back to the users page

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import UserTable from './UserTable';
+import Link from 'next/link';
 
 interface Props {
   searchParams: { sortOrder: string };
 }
 
+{
+  /* <p>{new Date().toLocaleTimeString()}</p> */
+}
 const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
-      {/* <p>{new Date().toLocaleTimeString()}</p> */}
+      <Link href='/users/new' className='btn '>
+        New User
+      </Link>
       <UserTable sortOrder={sortOrder} />
     </>
   );
