d8309793f628e263d31ce80c724c9852edd91d60
Scott Hale <sth0067@gmail.com>
2023-09-18 21:44:59 -0500

    added POST request to api-users-route file

--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -1,9 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export function GET(request: NextRequest) {
-    // fetch users from a dd
-    return NextResponse.json([
-        { id: 1, name: 'Scott' },
-        { id: 2, name: 'Katee' }
-    ])
+  // fetch users from a dd
+  return NextResponse.json([
+    { id: 1, name: 'Scott' },
+    { id: 2, name: 'Katee' },
+  ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Once we read the body of the request
+  // Need to Validate
+  // If invalid, return 400
+  // Else, return data
+
+  if (!body.name)
+    return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
