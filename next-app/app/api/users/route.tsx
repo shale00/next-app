74b7af32cbdea8125ae5942540c8c6ccc7be07ea
Scott Hale <sth0067@gmail.com>
2023-09-20 19:41:27 -0500

    updated post function for users

--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -19,12 +19,19 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  const user = await prisma.user.create({
+  const user = await prisma.user.findUnique({
+    where: { email: body.email }
+  })
+
+  if (user)
+    return NextResponse.json({ error: 'User already exists'}, { status: 400 })
+
+  const newUser = await prisma.user.create({
     data: {
       name: body.name,
       email: body.email
     }
   })
 
-  return NextResponse.json(user, { status: 201 });
+  return NextResponse.json(newUser, { status: 201 });
 }
