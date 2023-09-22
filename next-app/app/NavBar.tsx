12f30bafd28449cb8459a106b6814844f3438fe2
Scott Hale <sth0067@gmail.com>
2023-09-22 18:01:51 -0500

    updated navbar to use the react context hook useSession, converted the navbar component into a client component

--- next-app/app/NavBar.tsx
+++ next-app/app/NavBar.tsx
@@ -1,14 +1,24 @@
+'use client';
+// Because we are using react context, we need the file to use the client
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className='flex bg-slate-200 p-3 space-x-3'>
       <Link href={'/'} className='mr-5'>
         Next.js
       </Link>
       <Link href='/users'>Users</Link>
-      <Link href='/api/auth/signin'>Login</Link>
+      {status === 'loading' && <div>Loading...</div>}
+      {/* Because the status is authenticated we know for sure there is a user, therefore we can use the *!* at the end of user to tell the TypeScript compiler that we defintely have a user*/}
+      {status === 'authenticated' && <div>{session.user!.name}</div>}
+      {status === 'unauthenticated' && (
+        <Link href='/api/auth/signin'>Login</Link>
+      )}
     </div>
   );
 };
