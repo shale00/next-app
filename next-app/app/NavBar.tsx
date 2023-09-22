e2f5e75212ded0f04b53a798b0d0ff8d54f52982
Scott Hale <sth0067@gmail.com>
2023-09-22 02:49:06 -0500

    added google client keys to env file and added a Link to sign in on the NavBar

--- next-app/app/NavBar.tsx
+++ next-app/app/NavBar.tsx
@@ -7,7 +7,8 @@ const NavBar = () => {
       <Link href={'/'} className='mr-5'>
         Next.js
       </Link>
-      <Link href={'/users'}>Users</Link>
+      <Link href='/users'>Users</Link>
+      <Link href='/api/auth/signin'>Login</Link>
     </div>
   );
 };
