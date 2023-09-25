8a557e955e698b3aa8ee87c1bc1a06244b2610fd
Scott Hale <sth0067@gmail.com>
2023-09-25 15:10:51 -0500

    added Image to home page

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+import beach from '@/public/images/20220728_171126.jpg';
 
 
 export default async function Home() {
 
   return (
     <main>
+      <Image src={beach} alt='beach' />
       
     </main>
   );
