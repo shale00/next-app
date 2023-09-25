d18e445294ab5c0f82088c371eab499120c9d7f4
Scott Hale <sth0067@gmail.com>
2023-09-25 18:17:58 -0500

    updated home page to show remote image, added the image remotePatterns to next config file

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,13 +1,17 @@
-import Image from "next/image";
+import Image from 'next/image';
 import beach from '@/public/images/20220728_171126.jpg';
 
-
 export default async function Home() {
-
   return (
-    <main>
-      <Image src={beach} alt='beach' />
-      
+    <main className='relative h-screen'>
+      <Image
+        src='https://res.cloudinary.com/dujewkz0r/image/upload/v1695337125/hjdmnpw9p555jsrbueig.jpg'
+        alt='beach'
+        fill
+        className='object-cover'
+        
+        quality={100}
+      />
     </main>
   );
 }
