8d5dbb904b5a445e52c3e787b627cc95a676b923
Scott Hale <sth0067@gmail.com>
2023-09-24 23:38:05 -0500

    replaced models in prisma schema with the ones provided in the database adapters on the authjs website, updated the nextAuth route

--- next-app/app/api/auth/[...nextauth]/route.ts
+++ next-app/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,20 @@
+import prisma from '@/prisma/client';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { NextAuthOptions } from 'next-auth';
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: 'jwt',
+  },
 };
 
 const handler = NextAuth(authOptions);
