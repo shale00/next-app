9c815fc41857bbf320c45e6c41cd6bd608325e9d
Scott Hale <sth0067@gmail.com>
2023-09-26 01:13:01 -0500

    cleared home page again, added font to public folder and imported fonts on the root layout file, updated tailwind.config

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,16 +1,21 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter, Roboto} from 'next/font/google';
+import { Inter, Roboto } from 'next/font/google';
+import localFont from 'next/font/local';
 import NavBar from './NavBar';
 import AuthProvider from './auth/Provider';
 import GoogleAnalyticsScript from './GoogleAnalyticsScript';
-
 
 const inter = Inter({ subsets: ['latin'] });
 
 const roboto = Roboto({
   subsets: ['latin'],
-  weight: ['400', '500']
+  weight: ['400', '500'],
+});
+
+const poppins = localFont({
+  src: '../public/fonts/poppins-regular-webfont.woff2',
+  variable: '--font-poppins'
 })
 
 export const metadata: Metadata = {
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang='en' data-theme='winter'>
       <GoogleAnalyticsScript />
-      <body className={inter.className}>
+      <body className={poppins.variable}>
         <AuthProvider>
           <NavBar />
           <main className='p-5'>{children}</main>
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,27 +1,7 @@
-import Image from 'next/image';
-import beach from '@/public/images/20220728_171126.jpg';
-import Link from 'next/link';
-import ProductCard from './components/ProductCard';
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
-
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
-    <main className='relative h-screen'>
-      <h1>Hello{session && <span>, {session.user!.name}!</span>}</h1>
-      <Link href='/users'>Users</Link>
-      <ProductCard></ProductCard>
-
-      {/* <Image
-        src='https://res.cloudinary.com/dujewkz0r/image/upload/v1695337125/hjdmnpw9p555jsrbueig.jpg'
-        alt='beach'
-        fill
-        className='object-cover'
-        
-        quality={100}
-      /> */}
+    <main>
+      <h1 className='font-poppins'>Hello World</h1>
     </main>
   );
 }
