8525351642e2fec99a1125ba702e1cab28bfa2df
Scott Hale <sth0067@gmail.com>
2023-09-26 18:58:30 -0500

    updated home page: added lazy loading example for javascript library lodash

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,22 +1,26 @@
 'use client';
 // import { Metadata } from 'next';
 import { useState } from 'react';
-import dynamic from 'next/dynamic';
 
-const HeavyComponent = dynamic(
-  () => import('./components/HeavyComponent'),
-  { 
-    ssr: false,
-    loading: () => <p>Loading...</p> });
 
 export default function Home() {
-  const [isVisible, setVisible] = useState(false);
 
   return (
     <main>
       <h1>Hello World</h1>
-      <button onClick={() => setVisible(true)}>Show</button>
-      {isVisible && <HeavyComponent />}
+      <button onClick={async () => {
+        const _ = (await import('lodash')).default;
+
+        const users = [
+          {name: 'c'},
+          {name: 'b'},
+          {name: 'a'}
+        ];
+
+       const sorted =  _.orderBy(users, ['name'])
+       console.log('sorted array of users', sorted);
+      }}>Show</button>
+      
     </main>
   );
 }
