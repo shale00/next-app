c11bf7b0442c8ac48c4a2d8cc2cf80bf88bc4122
Scott Hale <sth0067@gmail.com>
2023-09-26 02:38:29 -0500

    added ssr property to dynamic function on home page, imported lodash module

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -4,7 +4,10 @@ import { useState } from 'react';
 import dynamic from 'next/dynamic';
 
 const HeavyComponent = dynamic(
-  () => import('./components/HeavyComponent'));
+  () => import('./components/HeavyComponent'),
+  { 
+    ssr: false,
+    loading: () => <p>Loading...</p> });
 
 export default function Home() {
   const [isVisible, setVisible] = useState(false);
