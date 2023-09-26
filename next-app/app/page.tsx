ea271e8f8cecca0a70be791f4012581bcb413011
Scott Hale <sth0067@gmail.com>
2023-09-26 01:37:45 -0500

    added metadata tag examples on the home page

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,7 +1,24 @@
+import { Metadata } from 'next';
+
 export default async function Home() {
   return (
     <main>
       <h1>Hello World</h1>
     </main>
   );
+}
+
+// Custom metadata for each file example:
+// export const metadata: Metadata = {
+//   title: '...'
+// }
+
+// To generate metadata dyncamically (for a product):
+export async function generateMetadata(): Promise<Metadata> {
+  const product = await fetch('');
+
+  return {
+    title: 'product.title',
+    description: 'product.description',
+  };
 }
